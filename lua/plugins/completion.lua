f2e08756826bf2ac062a1a2b3bdfaabf844a3d13
Fadta <fvpan01@gmail.com>
2025-08-17 02:43:50 -0300

    Added and integrated LSP

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'enter' },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = {
+        documentation = {
+          auto_show = false,
+          window = {
+            border = 'double',
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+          }
+        },
+        menu = {
+          border = 'bold',
+          draw = { gap = 2 },
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+        },
+
+        ghost_text = {
+          enabled = false,
+          show_with_menu = true
+        },
+        accept = {
+          auto_brackets = {enabled = true}
+        }
+      },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        }
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+  },
+  -- [[
+  -- #### AUTOPAIRS ####
+  -- ]]
+  { 'windwp/nvim-autopairs', event = "InsertEnter", config = true }
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,105 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'Saghen/blink.cmp'
+    },
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('ConfLspAttach', {clear = true}),
+        callback = function(event)
+          local client_id = event.data.client_id
+          local client = vim.lsp.get_client_by_id(client_id)
+
+          local function map(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, {buffer = event.buf, desc = 'LSP: ' .. desc})
+          end
+
+          -- KEYMAPS
+          local builtin = require'telescope.builtin'
+          map('gd', builtin.lsp_definitions, '[G]oto [d]efinition')
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('gr', builtin.lsp_references, '[G]oto [r]eferences under cursor')
+          map('gI', builtin.lsp_implementations, '[G]oto [I]mplementations')
+          map('<leader>lws', builtin.lsp_workspace_symbols, '[w]orkspace [s]ymbols')
+          map('<leader>lds', builtin.lsp_document_symbols, '[d]ocument [s]ymbols')
+          map('<leader>lr', vim.lsp.buf.rename, '[r]ename')
+          map('<leader>la', vim.lsp.buf.code_action, 'Code [a]ction')
+
+
+          -- autocomplete
+          --[[if client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client_id, event.buf, { autotrigger = true })
+          end
+          vim.cmd[[set completeopt+=noselect]]
+          --]]
+
+          -- AUTOCOMMANDS on attach
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+            local highlight_augroup = vim.api.nvim_create_augroup('ConfLspHighlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('ConfLspDetach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'conf-lsp-highlight', buffer = event2.buf }
+              end,
+            })
+          end
+        end -- END OF CALLBACK
+      })
+
+        local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
+        local diagnostic_signs = {}
+        for type, icon in pairs(signs) do
+          diagnostic_signs[vim.diagnostic.severity[type]] = icon
+        end
+        vim.diagnostic.config { signs = { text = diagnostic_signs } }
+    end,
+  }, -- End of lspconfig
+
+
+--[[
+--  ###### MASON ######
+--]]
+  {
+    'mason-org/mason-lspconfig.nvim',
+    opts = {
+      ui = {
+        border = 'rounded'
+      }
+    },
+    dependencies = {
+      { 'mason-org/mason.nvim', opts = {}},
+      'neovim/nvim-lspconfig',
+    },
+    config = function()
+        require'mason-lspconfig'.setup {
+          ensure_installed = {'lua_ls'},
+          handlers = {
+            function(server_name)
+              vim.lsp.enable(server_name)
+            end
+          }
+        }
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        virtual_lines = false,
+        underline = true,
+      })
+    end
+  }
+}
