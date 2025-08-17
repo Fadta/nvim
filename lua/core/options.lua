59b1c7a1cde956a06ba9ba7ebb6259b261278005
Fadta <fvpan01@gmail.com>
2025-08-17 02:23:05 -0300

    Added new options to core/options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,12 +1,23 @@
 -- INPUT OPTIONS
 vim.g.mapleader = ' '
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.opt.clipboard = 'unnamedplus' -- Shared clipboard (remember to install the system utility, like xclip for x11 or wl-clipboard for wayland
 
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
 vim.opt.swapfile = false
+vim.opt.showmode = false
+
+-- Folds
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.wo.foldlevel = 4
+
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.wrap = false
-vim.opt.cursorline = true -- Highlight cursor line
 vim.opt.scrolloff = 10
 
 -- Case sensitivity of searching
@@ -18,3 +29,12 @@ vim.opt.inccommand = 'split'
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+-- Appearance
+vim.opt.winborder = 'rounded'
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.wo.fillchars = 'eob: ' -- removes tildes '~' replacing numbers in nonlines
+vim.opt.termguicolors = true
+vim.o.laststatus = 3
