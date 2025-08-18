8dd94a3f50e3c81a4bc1b58c8d24c7301f70368b
Fadta <fvpan01@gmail.com>
2025-08-17 22:10:46 -0300

    Added oil, yazi and nvim-tree to interface with filesystem
    Yazi is now an external dependency

---
+++ lua/plugins/file_navigation.lua
@@ -0,0 +1,92 @@
+return {
+---@type LazySpec
+  {
+    "mikavilpas/yazi.nvim",
+    version = "*", -- use the latest stable version
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>-",
+        mode = { "n", "v" },
+        "<cmd>Yazi toggle<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+      -- mark netrw as loaded so it's not loaded at all.
+      --
+      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+      vim.g.loaded_netrwPlugin = 1
+    end,
+  },
+
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+  },
+
+  {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local tree = require'nvim-tree'
+    tree.setup {
+      renderer = {
+        group_empty = true,
+        -- search for glyphs on nvim-tree docs
+        icons = {
+          glyphs = {
+            git = {
+              unstaged = "󰛑",
+              staged = "󰛐",
+              unmerged = "",
+              renamed = "󰬳",
+              untracked = "",
+              deleted = "",
+              ignored = "󱀣",
+            }
+          }
+        },
+      },
+
+      sort = {
+        sorter = "case_sensitive",
+      },
+      view = {
+        signcolumn = 'auto',
+        width = 30,
+      },
+      filters = {
+        dotfiles = true,
+      },
+    }
+  end
+  }
+}
