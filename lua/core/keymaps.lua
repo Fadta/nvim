aff334187f2f53b075bc3b4e6d8d7fb5e9401819
Fadta <fvpan01@gmail.com>
2025-08-17 22:12:56 -0300

    New keymaps for plugins and QoL

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,6 +14,25 @@ nmap('<leader>o', '<cmd>update<CR><cmd>source<CR>', { desc = "Save and source co
 nmap('<leader>w', '<cmd>write<CR>', { desc = '[w]rite' })
 nmap('<leader>q', '<cmd>quit<CR>', { desc = '[q]uit' })
 
+-- Better block indentation by reapplying selection
+vmap('<', '<gv')
+vmap('>', '>gv')
+
+-- Easier buffer jumps
+nmap('<C-h>', '<C-w>h')
+nmap('<C-j>', '<C-w>j')
+nmap('<C-k>', '<C-w>k')
+nmap('<C-l>', '<C-w>l')
+
+-- Movement
+nmap('<C-Up>', '<C-u>')
+nmap('<C-Down>', '<C-d>')
+
+-- Show plugin interfaces
+nmap('<leader>st', '<cmd>NvimTreeToggle<CR>', {desc = '[s]how [t]ree'})
+nmap('<leader>sy', '<cmd>Yazi<CR>', {desc = '[s]how [y]azi'})
+nmap('<leader>so', '<cmd>Oil --float<CR>', {desc = '[s]how [o]il'})
+
 -- Quickly return to normal mode with movement keys
 imap('jk', '<Esc>')
 imap('kj', '<Esc>')
