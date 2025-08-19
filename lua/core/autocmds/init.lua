f41bb443aa9617c6a97d24ff8d7a72c05ee1735d
Fadta <fvpan01@gmail.com>
2025-08-19 19:54:00 -0300

    New function GetProjectRoot for a frequent query

--- lua/core/autocmds/init.lua
+++ lua/core/autocmds/init.lua
@@ -7,6 +7,29 @@ function Autocmd(event, opts)
   vim.api.nvim_create_autocmd(event, opts)
 end
 
+
+function GetProjectRoot(source)
+  vim.fs.root(source, function(name, path)
+      local pattern = { ".git", "Cargo.toml", "go.mod" }
+      local abspath = { vim.fn.stdpath("config") }
+      local parentpath = { "~/.config", "~/projects" }
+
+      return vim.iter(pattern):any(function(filepat)
+        return filepat == name
+      end)
+
+      or vim.iter(abspath):any(function(dirpath)
+        return vim.fs.normalize(dirpath) == path
+      end)
+
+      or vim.iter(parentpath):any(function(ppath)
+        return vim.fs.normalize(ppath) == vim.fs.dirname(path)
+      end)
+    end)
+
+end
+
+
 Autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = Augroup('HighlightYank'),
@@ -30,34 +53,12 @@ Autocmd("BufReadPost", {
 Autocmd("BufWinEnter", {
   desc = "auto change local current directory",
   group = Augroup("auto-project-root"),
-  callback = function(args)
-    if vim.api.nvim_get_option_value("buftype", { buf = args.buf }) ~= "" then
+  callback = function(event)
+    if vim.api.nvim_get_option_value("buftype", { buf = event.buf }) ~= "" then
       return
     end
 
-    local root = vim.fs.root(args.buf, function(name, path)
-      local pattern = { ".git", "Cargo.toml", "go.mod" }
-      local multipattern = { "build/compile_commands.json" }
-      local abspath = { vim.fn.stdpath("config") }
-      local parentpath = { "~/.config", "~/prj" }
-
-      return vim.iter(pattern):any(function(filepat)
-        return filepat == name
-      end)
-
-      or vim.iter(multipattern):any(function(filepats)
-        return vim.uv.fs_stat(vim.fs.joinpath(path, vim.fs.normalize(filepats)))
-      end)
-
-      or vim.iter(abspath):any(function(dirpath)
-        return vim.fs.normalize(dirpath) == path
-      end)
-
-      or vim.iter(parentpath):any(function(ppath)
-        return vim.fs.normalize(ppath) == vim.fs.dirname(path)
-      end)
-    end)
-
+    local root = GetProjectRoot(event.buf)
 
     if root then
       vim.cmd.lcd(root)
