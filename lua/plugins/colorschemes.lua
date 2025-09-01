e206b4f180ef872242ccd18a5ef3fa59baa61d6e
Fadta <fvpan01@gmail.com>
2025-08-31 21:42:30 -0300

    Moved plugins to new plugins/looknfeel.lua

--- lua/plugins/colorschemes.lua
+++
@@ -1,5 +0,0 @@
-return {
-  { "ellisonleao/gruvbox.nvim", priority = 1000, lazy = false, config = true },
-  { 'vague2k/vague.nvim', priority = 1000, lazy = true },
-  { 'EdenEast/nightfox.nvim', priority = 1000, lazy = true },
-}
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,26 +1,5 @@
 return {
   'tpope/vim-sleuth', -- Detection of tabstop and shiftwidth automatically
-  'nvim-tree/nvim-web-devicons', -- Icons
-
-  -- Look and feel
-  {
-    'sphamba/smear-cursor.nvim',
-    opts = {},
-  },
-  {
-    'karb94/neoscroll.nvim',
-    opts = {},
-  },
-  {
-    'nvimdev/indentmini.nvim',
-    config = function ()
-      require'indentmini'.setup()
-      -- There is no default value.
-      vim.cmd.highlight('IndentLine guifg=#404040')
-      -- Current indent line highlight
-      vim.cmd.highlight('IndentLineCurrent guifg=#606060')
-    end
-  },
 
   -- Integrate notifications to the UI
   {
