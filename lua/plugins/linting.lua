e007d5717534b95c5953c8a23b6378eee79c6bc2
Fadta <fvpan01@gmail.com>
2025-09-02 17:26:06 -0300

    Plugins: Added conform + nvim-lint. Config for selene and stylua

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
+        lua = { "selene" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("Config-linter", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+}
